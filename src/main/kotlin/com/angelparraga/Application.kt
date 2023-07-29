d75aa7e5298807b468792a36b32c4f3e75c950b5
Ángel Párraga Muñoz <im.angelparraga@gmail.com>
2023-07-29 03:30:57 +0200

    Add authentication to existing endpoints.

--- src/main/kotlin/com/angelparraga/Application.kt
+++ src/main/kotlin/com/angelparraga/Application.kt
@@ -12,8 +12,8 @@ fun main(args: Array<String>): Unit =
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     configureSerialization()
-    configureRouting()
     configureKoin()
     authentication()
+    configureRouting()
 }
 
--- src/main/kotlin/com/angelparraga/plugins/Routing.kt
+++ src/main/kotlin/com/angelparraga/plugins/Routing.kt
@@ -3,15 +3,18 @@ package com.angelparraga.plugins
 import com.angelparraga.routes.ntApiRouting
 import com.angelparraga.routes.nuclearRouting
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        ntApiRouting()
-        nuclearRouting()
-        get("/") {
-            call.respondText("Hello World!")
+        authenticate {
+            ntApiRouting()
+            nuclearRouting()
+            get("/") {
+                call.respondText("Hello World!")
+            }
         }
     }
 }
