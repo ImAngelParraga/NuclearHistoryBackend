290ae7b5009588a254ccff52d51273add0d32438
Angel Parraga <im.angelparraga@gmail.com>
2023-11-27 01:28:33 +0100

    Add CORS plugin.

---
+++ src/main/kotlin/com/angelparraga/plugins/CORS.kt
@@ -0,0 +1,25 @@
+package com.angelparraga.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+
+fun Application.configureCORS() {
+    install(CORS) {
+        anyHost()
+
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowHeader(HttpHeaders.ContentType)
+
+        allowCredentials = true
+        allowNonSimpleContentTypes = true
+    }
+}
